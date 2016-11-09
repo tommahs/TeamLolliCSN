1b2980b7f5409bb4ca1532e36cef9849094df199
JasperKalf <jasp3r.kalf@gmail.com>
2016-11-09 17:23:57 +0100

    De upload  bestanden

--- Configuration map/changePinCode.py
+++ Configuration map/changePinCode.py
@@ -4,16 +4,10 @@ import csv
 
 file = 'pincode.csv'
 
-# Writing lockerFile
-# def writeToFile(file, pincode):
-#     try:
-#         with open(file, 'w', newline='') as myCSVFile:
-#             writer = csv.writer(myCSVFile, delimiter=';')
-#             for pincode in file.items():
-#                 writer.writerow((pincode))
-#
-#     except Exception as ex:
-#         print(ex)
+
+List= []
+
+
 
 def readFromFile(file):
     with open(file, newline='') as f:
@@ -22,19 +16,29 @@ def readFromFile(file):
            content = (row[0])
         return content
 
-
+# def writeFromFile(file):
+#     with open(file, 'w', newline='') as f:
+#         writer = csv.writer(f)
+#         content = readFromFile()
+#         for inputNew in content:
+#             writer.writerow(inputNew)
 
 ## Change pincode functie
 def changPinCode(file):
     inputOld = (input('uw oude pincode?: '))
     content = readFromFile(file)
     if content == inputOld:
-        inputNew = (str(input('Voer hier uw nieuwe pincode in: ')))
+        inputNew = ((input('Voer hier uw nieuwe pincode in: ')))
+        pincodeFile = open(file, 'w')
+        writer = csv.writer(pincodeFile)
+        ##   \/ \/ \/ \/ \/ \/  ##
+        writer.writerow(inputNew)
+        ##^^^^^^^^^^^^^^^^^^^^^^^ is het problem
 
-        with open(file, 'w', newline=inputNew) as f:
-            writer = csv.writer(f)
-            for inputNew in content:
-                writer.writerow(inputNew)
+        # with open(file, 'w', newline='') as f:
+        #     writer = csv.writer(f)
+        #     for inputNew in content:
+        #         writer.writerow(inputNew)
 
     else:
         print ('Verkeerde pincode. Voer de goed pincode in.'),changPinCode(file)
