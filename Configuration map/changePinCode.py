9631714386f26a0cfaf8a473792d1242986665af
JasperKalf <jasp3r.kalf@gmail.com>
2016-11-09 16:35:49 +0100

    De upload  bestanden

--- Configuration map/changePinCode.py
+++ Configuration map/changePinCode.py
@@ -1,17 +1,43 @@
 ## Here we will change the pincode ##
-pincode = 1234
+import csv
+
+
+file = 'pincode.csv'
+
+# Writing lockerFile
+# def writeToFile(file, pincode):
+#     try:
+#         with open(file, 'w', newline='') as myCSVFile:
+#             writer = csv.writer(myCSVFile, delimiter=';')
+#             for pincode in file.items():
+#                 writer.writerow((pincode))
+#
+#     except Exception as ex:
+#         print(ex)
+
+def readFromFile(file):
+    with open(file, newline='') as f:
+        reader = csv.reader(f)
+        for row in reader:
+           content = (row[0])
+        return content
 
 
 
-def changPinCode(pincode):
-    inputOld = int(input('wat is uw oude pincode?: '))
-    if inputOld == pincode:
-        inputNew = int(input('Voer hier uw nieuwe pincode in: '))
-        pincode = inputNew
-        return pincode
+## Change pincode functie
+def changPinCode(file):
+    inputOld = (input('uw oude pincode?: '))
+    content = readFromFile(file)
+    if content == inputOld:
+        inputNew = (str(input('Voer hier uw nieuwe pincode in: ')))
+
+        with open(file, 'w', newline=inputNew) as f:
+            writer = csv.writer(f)
+            for inputNew in content:
+                writer.writerow(inputNew)
 
     else:
-        print ('Verkeerde pincode. Voer de goed pincode in.'),changPinCode()
+        print ('Verkeerde pincode. Voer de goed pincode in.'),changPinCode(file)
 
-chpincode = changPinCode(pincode)
-print(chpincode)
+changPinCode(file)
+
